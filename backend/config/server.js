55b79231f83ecc2fe69a8a5aadc87cb007130778
Tom K <36858744+decentralvision@users.noreply.github.com>
2021-07-19 04:01:38 -0700

    Create Strapi starter project

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '30e32b7edecffa8dd6fa2eb7f28f9704'),
+    },
+  },
+});
